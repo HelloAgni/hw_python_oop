29050f62e4020092f52d474bc4f5ae51789187b7
Байбак Кирилл <zloifox@gmail.com>
2021-11-06 12:15:34 +0300

    конструктор класса InfoMessage

--- homework.py
+++ homework.py
@@ -1,11 +1,31 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    pass
+    
+    def __init__(self,
+                 training_type: str,
+                 duration: float,
+                 distance: float,
+                 speed: float,
+                 calories: float) -> None:
+        self.training_type = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
+    def get_message(self):
+        """Метод выводит возвращает строку сообщения."""
 
+        return(f'Тип тренировки: {self.training_type}; Длительность: {self.duration:.3f} ч.; '
+               f'Дистанция: {self.distance:.3f} км; Ср. скорость: {self.speed:.3f} км/ч; '
+               f'Потрачено ккал: {self.calories:.3f}.')
 
 class Training:
     """Базовый класс тренировки."""
-
+    
+    M_IN_KM: int = 1000
+    LEN_STEP: float = 0.65 #1.38
+    coeff_calories_1 = 18
+    coeff_calories_2 = 20
     def __init__(self,
                  action: int,
                  duration: float,
@@ -52,7 +72,8 @@ def read_package(workout_type: str, data: list) -> Training:
 
 def main(training: Training) -> None:
     """Главная функция."""
-    def show_training_info()  # 05.11 Kir
+    def show_training_info():
+        pass
 
 
 if __name__ == '__main__':
